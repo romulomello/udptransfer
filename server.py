e2d0c3d56bfb7e43518241e51806b15dbaeb6d9f
Antonio <antonio.medeiros@estudante.ufjf.br>
2023-07-16 22:05:28 -0300

    add timestamp nos logs

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import random
 import logging
 
 # Set up logging configuration
-logging.basicConfig(level=logging.INFO, filename='server.log', filemode='w', format='%(message)s')
+logging.basicConfig(level=logging.INFO, filename='server.log', filemode='w', format='%(asctime)s - %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger()
 logger.addHandler(logging.StreamHandler())
 
